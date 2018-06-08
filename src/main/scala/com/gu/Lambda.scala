97606f2dfad0f4f347f58f65f81c493faf561da8
jharewinton <jonathan.hare-winton@guardian.co.uk>
2018-06-08 14:14:47 +0100

    add tests for hangouts endpoint logic

--- src/main/scala/com/gu/Lambda.scala
+++ src/main/scala/com/gu/Lambda.scala
@@ -52,19 +52,20 @@ object Lambda {
     AWS.s3Client.putObject(Config.s3Bucket, Config.s3Key, labels.mkString(","))
   }
 
-  def notifyHangouts(label: String, isDelete: Boolean = false) = {
-    val hangoutsEndpoint = {
-      if (Config.appsLabelPrefixes.contains(label.stripPrefix("\"").stripSuffix("\"").take(3))) {
-        logger.info(s"Using apps webhook for $label")
-        Config.appsHangoutsUrl
-      } else {
-        logger.info(s"Using userhelp webhook for $label")
-        Config.hangoutsUrl
-      }
+  def getHangoutsEndpoint(label: String) = {
+    if (Config.appsLabelPrefixes.contains(label.stripPrefix("\"").stripSuffix("\"").take(3))) {
+      logger.info(s"Using apps webhook for $label")
+      Config.appsHangoutsUrl
+    } else {
+      logger.info(s"Using userhelp webhook for $label")
+      Config.hangoutsUrl
     }
+  }
+
+  def notifyHangouts(label: String, isDelete: Boolean = false) = {
     def hangoutsPost(message: String): Response = {
       val post = new Request.Builder()
-        .url(hangoutsEndpoint)
+        .url(getHangoutsEndpoint(label))
         .post(RequestBody.create(MediaType.parse("application/json; charset=utf-8"), Json.toJson(HangoutsPayload(message)).toString()))
         .build()
       http.newCall(post).execute()
